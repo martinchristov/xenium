75e150a1bc0a6dcda776045b6659d8468a6ee843
Martin Christov <martin.christov@gmail.com>
2014-02-04 14:28:41 +0200

    background now changing

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,32 @@
-$(document).ready(function(){
+var $sections=[],
+	sectiony={},
+	$doc=$(document),
+	lastsection = '';
+$doc.ready(function(){
 	$(".nav a").click(function(e){
 		e.preventDefault();
-		var $target = $($(this).attr('href'));
-		$(document).scrollTo($target.offset().top-200,500);
+		var href = $(this).attr('href')
+		var $target = $(href);
+		$doc.scrollTo($target.offset().top-200,500);
+	})
+	$sections = $('.section-outer');
+	$(window).resize();
+})
+
+$doc.scroll(function(e){
+	var y = $doc.scrollTop(),
+		section = '';
+	for(var o in sectiony){
+		if(y>sectiony[o])section=o;
+	}
+	if(section!=lastsection){
+		lastsection=section;
+		$("body").attr('class',section);
+	}
+})
+
+$(window).resize(function(){
+	$sections.each(function(){
+		sectiony[$(this).attr('id')] = $(this).position().top;
 	})
 })
